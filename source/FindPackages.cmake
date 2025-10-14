86d9d90492be80f30158e752b31aae28538be8cf
jelemiekun[laptop-linux] <jeremieeden1946@gmail.com>
2025-10-14 14:14:33 +0800

    prac: simple main.cpp

---
+++ source/FindPackages.cmake
@@ -0,0 +1,17 @@
+include_guard()
+
+message("-- Hello from ${CMAKE_CURRENT_LIST_FILE}")
+
+function(find_packages)
+  message("-- Finding packages...")
+
+  message("-- Finding spdlog package...")
+  find_package(spdlog REQUIRED)
+
+  if (TARGET spdlog::spdlog)
+    message("-- spdlog package exists!")
+    set(READY_TO_COMPILE TRUE PARENT_SCOPE)
+  else()
+    message("-- spdlog package failed to find!")
+  endif()
+endfunction()
--- source/src/main.cpp
+++ source/src/main.cpp
@@ -1,8 +1,6 @@
-#include "Game.h"
 #include <spdlog/spdlog.h>
 
 int main(int argc, char *argv[]) {
   spdlog::info("Hello from main.cpp!");
-  Game::greet();
   return 0;
 }
