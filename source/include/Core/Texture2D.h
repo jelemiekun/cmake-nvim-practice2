4e7d0eca60875cd90f8179f74b1fdd289f12e92b
jelemiekun[laptop-linux] <jeremieeden1946@gmail.com>
2025-10-22 11:50:36 +0800

    add: ebo, texture2d, vao, stb_image

---
+++ source/include/Core/Texture2D.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <string>
+
+class Texture2D {
+private:
+  unsigned int rendererID;
+  std::string filePath;
+  unsigned char *localBuffer;
+  int width;
+  int height;
+  int bpp;
+
+public:
+  Texture2D(const std::string &path);
+  ~Texture2D();
+
+  void bind(unsigned int slot = 0) const;
+  void unbind() const;
+
+  inline int getWidth() const { return width; }
+  inline int getHeight() const { return height; }
+};
