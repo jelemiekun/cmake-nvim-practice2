7e699f686ea2532cffb41cb2f862e5bdade03c1b
jelemiekun[laptop-linux] <jeremieeden1946@gmail.com>
2025-10-28 16:02:20 +0800

    fix: glsl

--- source/shaders/source.glsl
+++ source/shaders/source.glsl
@@ -3,8 +3,20 @@
 
 layout(location = 0) in vec3 L_coordinate;
 layout(location = 1) in vec3 L_normal;
+layout(location = 2) in vec3 L_texCoord;
 
 uniform vec3 u_NormalMatrix;
 uniform mat4 u_Projection;
 uniform mat4 u_View;
 uniform mat4 u_Model;
+
+out vec3 v_Normal;
+out vec2 v_TexCoord;
+
+void main() {
+    mat4 mvp = u_Projection * u_View * u_Model;
+    gl_Position = mvp * vec4(L_coordinate, 1.0f);
+
+    v_Normal = mat3(transpose(inverse(u_Model))) * L_normal;
+    v_TexCoord = L_texCoord;
+}
